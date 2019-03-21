8606a19c245195c1cbf877af17deb67925ccc5e1
ehchoi <ehchoi@cafe24corp.com>
2019-03-21 15:53:03 +0900

    optional

--- src/main/java/com/silverhyuk/telegrambbot/bot/ZerosllBot.java
+++ src/main/java/com/silverhyuk/telegrambbot/bot/ZerosllBot.java
@@ -1,15 +1,21 @@
 package com.silverhyuk.telegrambbot.bot;
 
 import com.silverhyuk.telegrambbot.properties.ProjectProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Optional;
 
 @Component
 public class ZerosllBot extends TelegramLongPollingBot {
 
+    Logger log = LoggerFactory.getLogger(this.getClass());
     private final ProjectProperties projectProperties;
 
     public ZerosllBot(ProjectProperties projectProperties) {
@@ -18,7 +24,12 @@ public class ZerosllBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage());
+        Message message = update.getMessage();
+        String userLastName = Optional.ofNullable(message.getFrom().getLastName()).orElse("");
+        String userFirstName = Optional.ofNullable(message.getFrom().getFirstName()).orElse("");
+
+        log.info("{} : {}", userLastName+userFirstName, message.getText());
+        //System.out.println(update.getMessage());
         SendMessage sn = new SendMessage();
         sn.setChatId(update.getMessage().getChatId());
         sn.setText(update.getMessage().getText());
